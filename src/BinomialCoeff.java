a005d2b37333615e1bae4eeeee43d163255050bb
rakinplaban <rakinshahriar54@gmail.com>
2023-03-13 18:11:23 +0600

    Done

--- src/BinomialCoeff.java
+++ src/BinomialCoeff.java
@@ -1,11 +1,14 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class BinomialCoeff extends Factorial{
     public int p, q, r, s;
-    private double first, second;
-    public double combination(){
-        first = factorial(p)/(factorial(p-q)*factorial(q));
-        second = factorial(r)/(factorial(r-s)*factorial(s));
+    private BigDecimal first, second;
+    public BigDecimal combination(){
+        first = factorial(p).divide(factorial(p-q).multiply(factorial(q)),5, RoundingMode.HALF_UP);
+        second = factorial(r).divide(factorial(r-s).multiply(factorial(s)),5,RoundingMode.HALF_UP);
 
-        return first/second;
+        return first.divide(second,5,RoundingMode.HALF_UP);
     }
 
 }
--- src/Factorial.java
+++ src/Factorial.java
@@ -1,11 +1,17 @@
+import java.math.BigDecimal;
+import java.math.BigInteger;
 public class Factorial {
 //    private int fact;
+    private BigDecimal factor;
 
-    public double factorial(int a){
+
+    public BigDecimal factorial(long a){
+        factor = BigDecimal.valueOf(a);
         if(a==0)
-            return 1;
+            return BigDecimal.ONE;
         else{
-            return a * factorial(a-1);
+//            return a * factorial(a-1);
+            return factor.multiply(factorial(a-1));
         }
     }
 }
